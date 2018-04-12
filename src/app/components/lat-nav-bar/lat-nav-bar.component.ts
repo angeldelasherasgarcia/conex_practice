636cbb4a5eda36db628f973fc7dee1c0edae1be9
aherasg <aherasg@indra.es>
2018-04-12 10:01:32 +0200

    [CONEX] Añado css a la parte principal de la aplicacion

--- src/app/components/lat-nav-bar/lat-nav-bar.component.ts
+++ src/app/components/lat-nav-bar/lat-nav-bar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class LatNavBarComponent implements OnInit {
   public nombreUsuario : String;
   public centro : String;
-  public isActive : String;
+  public isActive : String = 'convenios';
   constructor() { }
   ngOnInit() {
     this.nombreUsuario="NOMBRE USUARIO"
