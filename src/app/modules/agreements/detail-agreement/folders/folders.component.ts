1c5f905528b6ee463f8a8449eddcf69be22dcc66
Ángel de las Heras García <aherasg@indra.es>
2018-04-24 14:34:50 +0200

    [CONEX] Se añade funcionalidad a la ventana modal de descargas

--- src/app/modules/agreements/detail-agreement/folders/folders.component.ts
+++ src/app/modules/agreements/detail-agreement/folders/folders.component.ts
@@ -10,31 +10,57 @@ import { ModalTwoButtonComponent } from '../../../../layouts/modal-two-button/mo
 export class FoldersComponent implements OnInit {
   private modal: any;
   constructor(public conveniosService: ConveniosService,
-              private modalService: NgbModal) { }
+    private modalService: NgbModal) { }
 
   ngOnInit() {
   }
+  /**
+   * @description Funcion que se llama al pulsar el icono del pdf
+   * en la tabla. 
+   * Se llevan a cabo las siguientes acciones :
+   * 1. Se abre el modal donde el usuario puede eleguir la acción a realizar.
+   * 2. Si la acción es guardar o mostrar , se llama al servicio de descarga del pdf.
+   * 3. Una vez llamado al servicio se muestra o se descarga el pdf en función del
+   * boton que el usuario haya pulsado en el modal.
+   */
   public downloadFile(): any {
-    this.conveniosService.getFile().subscribe(
-      result => {
-        this.modal = this.modalService.open(ModalTwoButtonComponent);
-        //this.downLoadCurrentFile(result);
-      },
-      error => {
-        console.log(error);
+    this.modal = this.modalService.open(ModalTwoButtonComponent);
+    this.modal.result.then((resultModal) => {
+      if (resultModal !== '') {
+        this.conveniosService.getFile().subscribe(
+          result => {
+            if (resultModal === 'guardar') {
+              this.downLoadCurrentFile(result);
+            };
+            if (resultModal === 'abrir') {
+              this.showCurrentFile(result)
+            };
+          },
+          error => {
+            console.log(error);
+          }
+        )
       }
-    )
+    })
   }
+  /**
+   * 
+   * @param blob Archivo que se va a descargar
+   * @description Descargar del archivo de pdf 
+   */
   public downLoadCurrentFile(blob): void {
     var a = document.createElement("a");
     document.body.appendChild(a);
-
     var url = window.URL.createObjectURL(blob);
     a.href = url;
     a.download = 'pdffile';
-    //console.log(url)
     a.click();
   }
+  /**
+   * 
+   * @param blob Archivo que se va a descargar
+   * @description Muestra el pdf en otra pantalla
+   */
   public showCurrentFile(blob): void {
     var url = window.URL.createObjectURL(blob);
     window.location.assign(url);
