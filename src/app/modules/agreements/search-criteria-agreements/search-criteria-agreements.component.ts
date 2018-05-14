093ce59981057a8c48ede50e34a8acfbdca6a8d2
Ángel de las Heras García <aherasg@indra.es>
2018-05-14 09:02:26 +0200

    [CONEX] Añado nuevos estilos para el contador

--- src/app/modules/agreements/search-criteria-agreements/search-criteria-agreements.component.ts
+++ src/app/modules/agreements/search-criteria-agreements/search-criteria-agreements.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm, FormControl, FormGroup, FormBuilder } from '@angular/forms'
-import { RegistroConvenios } from './../../../models/convenios.model';
-import { Router } from '@angular/router';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { ConveniosService } from './../../../services/convenios/convenios.service';
-import { ErrorComponent } from '../../../layouts/error/error.component';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm, FormControl, FormGroup, FormBuilder } from "@angular/forms";
+import { RegistroConvenios } from "./../../../models/convenios.model";
+import { Router } from "@angular/router";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { ConveniosService } from "./../../../services/convenios/convenios.service";
+import { ErrorComponent } from "../../../layouts/error/error.component";
 
 @Component({
-  selector: 'app-search-criteria-agreements',
-  templateUrl: './search-criteria-agreements.component.html',
-  styleUrls: ['./search-criteria-agreements.component.css']
+  selector: "app-search-criteria-agreements",
+  templateUrl: "./search-criteria-agreements.component.html",
+  styleUrls: ["./search-criteria-agreements.component.css"]
 })
 export class SearchCriteriaAgreementsComponent implements OnInit {
   formpro: FormGroup;
@@ -20,12 +20,14 @@ export class SearchCriteriaAgreementsComponent implements OnInit {
   public materia: any = [];
   public hiddenOrgano: boolean = false;
   private modal: any;
+  public isValidateDate: boolean = true;
   //si es numerico iniciarlo como null
-  constructor(private convenioForm: FormBuilder,
+  constructor(
+    private convenioForm: FormBuilder,
     public router: Router,
     public conveniosService: ConveniosService,
-    private modalService: NgbModal) {
-  }
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.initObject();
@@ -34,36 +36,36 @@ export class SearchCriteriaAgreementsComponent implements OnInit {
 
   searchCriteria() {
     this.conveniosService.searchConvenio(this.convenio);
-    this.router.navigate(['list_agreements']);
+    this.router.navigate(["list_agreements"]);
   }
   hiddenDefaultOption() {
     this.hiddenOrgano = true;
   }
   initObject() {
     this.convenio = {
-      titulo: '',
-      organo_responsable: '',
-      desde_description_organo: '',
-      hasta_description_organo: '',
-      num_convenio_1: '',
-      num_convenio_2: '',
-      num_convenio_3: '',
-      num_convenio_4: '',
-      desde_importe_gastos: '',
-      hasta_importe_gastos: '',
-      desde_importe_ingresos: '',
-      hasta_importe_ingresos: '',
-      organos_firmantes: '',
-      entidades_firmantes: '',
-      tipo_entidades: '',
-      tipo_convenio: '',
-      materia: ''
+      titulo: "",
+      organo_responsable: "",
+      desde_description_organo: "",
+      hasta_description_organo: "",
+      num_convenio_1: "",
+      num_convenio_2: "",
+      num_convenio_3: "",
+      num_convenio_4: "",
+      desde_importe_gastos: "",
+      hasta_importe_gastos: "",
+      desde_importe_ingresos: "",
+      hasta_importe_ingresos: "",
+      organos_firmantes: "",
+      entidades_firmantes: "",
+      tipo_entidades: "",
+      tipo_convenio: "",
+      materia: ""
     };
   }
   callServices() {
     this.conveniosService.getCargaSelect().subscribe(
       result => {
-        this.tipoConvenio = result.tipoConvenio
+        this.tipoConvenio = result.tipoConvenio;
         this.tipoEntidad = result.tipoEntidad;
         this.organoRespnsable = result.responsibe;
         this.materia = result.materia;
@@ -71,11 +73,18 @@ export class SearchCriteriaAgreementsComponent implements OnInit {
       error => {
         this.modal = this.modalService.open(ErrorComponent);
         this.modal.componentInstance.data = "Oops..... 404 error";
-        this.modal.componentInstance.title_modal="Error"
-        this.modal.result.then((result) => {
-        })
+        this.modal.componentInstance.title_modal = "Error";
+        this.modal.result.then(result => {});
       }
-    )
+    );
   }
- 
+  public validateDate(): void {
+    if (this.convenio.hasta_description_organo) {
+      if (this.convenio.desde_description_organo > this.convenio.hasta_description_organo) {
+        this.isValidateDate = false;
+      } else {
+        this.isValidateDate = true;
+      }
+    }
+  }
 }
--- src/app/modules/count-down/count-down/count-down.component.ts
+++ src/app/modules/count-down/count-down/count-down.component.ts
@@ -1,47 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Rx";
 
 @Component({
-  selector: 'app-count-down',
-  templateUrl: './count-down.component.html',
-  styleUrls: ['./count-down.component.css']
+  selector: "app-count-down",
+  templateUrl: "./count-down.component.html",
+  styleUrls: ["./count-down.component.css"]
 })
 export class CountDownComponent implements OnInit {
-  public fecha : Date;
-  public dias:any;
-  public horas:any;
-  public minutos:any;
-  public segundos:any;
-  public fecha_value:string;
-  constructor() { }
+  public fecha: Date;
+  public dias: any;
+  public horas: any;
+  public minutos: any;
+  public segundos: any;
+  public fecha_value: string;
+  constructor() {}
 
-  ngOnInit() {;
-    this.fecha = new Date(
-      2019,3,27,0,0,0,0
-    )
-    let timer = Observable.timer(1000,1000);
-    timer.subscribe(t=>{
+  ngOnInit() {
+    this.fecha = new Date(2019, 3, 27, 0, 0, 0, 0);
+    let timer = Observable.timer(1000, 1000);
+    timer.subscribe(t => {
       this.countdown();
-      console.log('a')
     });
   }
-  public countdown():void{
+  public countdown(): void {
     //var fecha=new Date('2012','1','10','21','00','00')
-    var hoy=new Date()
-    if (this.fecha>hoy){
-        var diferencia=(this.fecha.getTime()-hoy.getTime())/1000
-        this.dias=Math.floor(diferencia/86400)
-        diferencia=diferencia-(86400*this.dias)
-        this.horas=Math.floor(diferencia/3600)
-        diferencia=diferencia-(3600*this.horas)
-        this.minutos=Math.floor(diferencia/60)
-        diferencia=diferencia-(60*this.minutos)
-        this.segundos=Math.floor(diferencia)
+    var hoy = new Date();
+    if (this.fecha > hoy) {
+      var diferencia = (this.fecha.getTime() - hoy.getTime()) / 1000;
+      this.dias = this.setValue0(Math.floor(diferencia / 86400));
+      diferencia = diferencia - 86400 * this.dias;
+      this.horas = this.setValue0(Math.floor(diferencia / 3600));
+      diferencia = diferencia - 3600 * this.horas;
+      this.minutos = this.setValue0(Math.floor(diferencia / 60));
+      diferencia = diferencia - 60 * this.minutos;
+      this.setValue0(Math.floor(diferencia));
+      this.segundos = this.setValue0(Math.floor(diferencia));
 
-        this.fecha_value='Quedan ' + this.dias + ' Días, ' + this.horas + ' Horas, ' + this.minutos + ' Minutos, ' + this.segundos + ' Segundos'
+      this.fecha_value =
+        "QUEDAN" +
+        this.dias +
+        " DIAS" +
+        this.horas +
+        " HORAS" +
+        this.minutos +
+        " MINUTOS" +
+        this.segundos +
+        " SEGUNDOS";
+    } else {
+      this.fecha_value =
+        "Quedan " +
+        this.dias +
+        " D&iacute;as, " +
+        this.horas +
+        " Horas, " +
+        this.minutos +
+        " Minutos, " +
+        this.segundos +
+        " Segundos";
     }
-    else{
-      this.fecha_value='Quedan ' + this.dias + ' D&iacute;as, ' + this.horas + ' Horas, ' + this.minutos + ' Minutos, ' + this.segundos + ' Segundos'
+  }
+  public setValue0(time) {
+    if (time < 10) {
+      return ("0"+time);
+    }else{
+      return time;
     }
-}
+  }
 }
